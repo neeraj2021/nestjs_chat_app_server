7b83f554d3a403f6f5790561601fc270dba72145
Neeraj Kumar <neerajkmahawar2017@gmail.com>
2023-01-22 15:46:50 +0530

    dev: add hostinger db cred

--- src/db/dbConnector.ts
+++ src/db/dbConnector.ts
@@ -1,13 +1,35 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user/user.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+function databaseCred() {
+  if (process.env.MODE == 'local') {
+    return {
+      HOST: 'localhost',
+      PORT: 3306,
+      USERNAME: 'root',
+      PASSWORD: 'Neeraj@123',
+      DATABASE: 'chat',
+    };
+  }
+  return {
+    HOST: process.env.CHAT_HOST,
+    PORT: process.env.CHAT_PORT,
+    USERNAME: process.env.CHAT_USER_NAME,
+    PASSWORD: process.env.CHAT_PASSWORD,
+    DATABASE: process.env.CHAT_DATABASE,
+  };
+}
 
 export const typeOrmModule = TypeOrmModule.forRoot({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'Neeraj@123',
-  database: 'chat',
+  host: databaseCred().HOST,
+  port: Number(databaseCred().PORT),
+  username: databaseCred().USERNAME,
+  password: databaseCred().PASSWORD,
+  database: databaseCred().DATABASE,
   entities: [User],
   synchronize: false,
   logging: false,
